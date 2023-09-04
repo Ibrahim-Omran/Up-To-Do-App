ce08ca6ff727a8916d63149e5db443353d12d369
Ibrahim-Omran <omranibraheem705@gmail.com>
2023-09-04 21:21:08 +0300

    navigate using future delayed

--- lib/features/auth/presentation/screens/splash_screen/splash_screen.dart
+++ lib/features/auth/presentation/screens/splash_screen/splash_screen.dart
@@ -2,37 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:to_do_app/core/utils/app_colors.dart';
 import 'package:to_do_app/core/utils/app_strings.dart';
+import 'package:to_do_app/features/auth/presentation/screens/on_boarding_screens/on_boarding_screens.dart';
 import '../../../../../core/utils/app_assets.dart';
 
-
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    navigate();
+  }
+
+  void navigate() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const OnBoardingScreens(),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: AppColors.background,
-
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(AppAssets.logo),
-              const SizedBox(
-                height: 19,
-              ),
-              Text(
-                AppStrings.appName,
+      backgroundColor: AppColors.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(AppAssets.logo),
+            const SizedBox(
+              height: 19,
+            ),
+            Text(AppStrings.appName,
                 style: GoogleFonts.lato(
                   color: AppColors.white,
                   fontSize: 40,
                   fontWeight: FontWeight.bold,
-                )
-
-              )
-            ],
-          ),
+                ))
+          ],
         ),
+      ),
     );
   }
 }
